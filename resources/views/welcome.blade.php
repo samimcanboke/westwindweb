554fc1517354f0253f25b21c6bb52fe927854155
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-06-24 13:30:03 +0300

    welcome change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Westwind Eissenbahnservice</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <script>
+            window.location.href = "/dashboard";
+        </script>
 
         <!-- Styles -->
         <style>
