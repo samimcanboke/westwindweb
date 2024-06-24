a74d9e48aa1af3ec37e1217bcda5d7467ca124d8
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-06-24 12:23:36 +0300

    change Finalize

--- app/Http/Controllers/FinalizedJobsController.php
+++ app/Http/Controllers/FinalizedJobsController.php
@@ -336,7 +336,8 @@ class FinalizedJobsController extends Controller
 
         $month = $request->month;
         $year = $request->year;
-        $user_id = $request->user()->id;
+        $user_id = $request->user->id;
+        dd($user_id);
         $startDate = Carbon::create($year, $month, 1)->startOfMonth();
         $endDate = Carbon::create($year, $month, 1)->endOfMonth();
         $data['year'] = $year;
