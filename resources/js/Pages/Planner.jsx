3e9f419cb2f4cb78ac91c77c30bc97d30c9c1df6
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-07-01 00:46:38 +0200

    add

--- resources/js/Pages/Planner.jsx
+++ resources/js/Pages/Planner.jsx
@@ -3,27 +3,45 @@ import { Head } from "@inertiajs/react";
 import "react-calendar-timeline/lib/Timeline.css";
 import moment from "moment";
 import { useState, useEffect } from "react";
-import { Accordion, AccordionPanel, AccordionTitle, AccordionContent, Label } from "flowbite-react";
+import {
+    Accordion,
+    AccordionPanel,
+    AccordionTitle,
+    AccordionContent,
+    Label,
+} from "flowbite-react";
 
 export default function Planner({ auth }) {
     const [jobs, setJobs] = useState([]);
     const getJobs = async () => {
-        await axios.get(route("get-user-job-plans")).then((response) => {
-            try {
-                console.log(response.data);
-                if(response.data.length > 0) {
-                    response.data.sort((a, b) => {
-                        return new Date(a.start_date + " " + a.start_time) - new Date(b.start_date + " " + b.start_time);
-                });
-                setJobs(response.data);
-                } 
-            } catch (error) {
+        await axios
+            .get(route("get-user-job-plans"))
+            .then((response) => {
+                try {
+                    if (response.status === 200) {
+                        if (response.data.length > 0) {
+                            response.data.sort((a, b) => {
+                                return (
+                                    new Date(
+                                        a.start_date + " " + a.start_time
+                                    ) -
+                                    new Date(b.start_date + " " + b.start_time)
+                                );
+                            });
+                            setJobs(response.data);
+                        } else {
+                            setJobs([]);
+                        }
+                    } else {
+                        console.log(response.response.data);
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            })
+            .catch((error) => {
                 console.log(error);
-            }
-            
-        }).catch((error) => {
-            console.log(error);
-        });
+            });
     };
     useEffect(() => {
         let id = setInterval(() => {
@@ -51,39 +69,70 @@ export default function Planner({ auth }) {
                             items={items}
                             defaultTimeStart={moment().add(-256, "hour")}
                             defaultTimeEnd={moment().add(256, "hour")}
-        />*/}           
-                        { jobs && jobs.map((job) => (
-                            <div key={job.id} style={{marginTop: "1rem", marginBottom: "1rem"}}>
-                                                        <Accordion>
-                                                            <AccordionPanel></AccordionPanel>
-                            <AccordionPanel isOpen={false}>
-                                <AccordionTitle>
-                                {moment(job.start_date + " " + job.start_time).format("DD.MM.YYYY HH:mm")} - {moment(job.end_date + " " + job.end_time).format("HH:mm")} : {job.from} - {job.to}
-                                </AccordionTitle>
-                                <AccordionContent>
-                                    <Label>Datum : </Label>
-                                    {moment(job.start_date + " " + job.start_time).format("DD.MM.YYYY HH:mm")} - {moment(job.end_date + " " + job.end_time).format("DD.MM.YYYY HH:mm")}
-                                    <br/>
-                                    <Label>Route : </Label>
-                                    {job.from} - {job.to}
-                                    <br/>
-                                    <Label>Loknummer : </Label>
-                                    {job.locomotive_nummer}
-                                    <br/>
-                                    <Label>Tour : </Label>
-                                    {job.tour_name}
-                                    <br/>
-                                    <Label>Zugnummer : </Label>
-                                    {job.zug_nummer}
-                                    <br/>
-                                    <Label>Kommentar : </Label>
-                                    {job.description}
-                                </AccordionContent>
-                            </AccordionPanel>
-                        </Accordion>
-                                
-                            </div>
-                        ))}
+        />*/}
+                        {jobs &&
+                            jobs.map((job) => (
+                                <div
+                                    key={job.id}
+                                    style={{
+                                        marginTop: "1rem",
+                                        marginBottom: "1rem",
+                                    }}
+                                >
+                                    <Accordion>
+                                        <AccordionPanel></AccordionPanel>
+                                        <AccordionPanel isOpen={false}>
+                                            <AccordionTitle>
+                                                {moment(
+                                                    job.start_date +
+                                                        " " +
+                                                        job.start_time
+                                                ).format(
+                                                    "DD.MM.YYYY HH:mm"
+                                                )}{" "}
+                                                -{" "}
+                                                {moment(
+                                                    job.end_date +
+                                                        " " +
+                                                        job.end_time
+                                                ).format("HH:mm")}{" "}
+                                                : {job.from} - {job.to}
+                                            </AccordionTitle>
+                                            <AccordionContent>
+                                                <Label>Datum : </Label>
+                                                {moment(
+                                                    job.start_date +
+                                                        " " +
+                                                        job.start_time
+                                                ).format(
+                                                    "DD.MM.YYYY HH:mm"
+                                                )}{" "}
+                                                -{" "}
+                                                {moment(
+                                                    job.end_date +
+                                                        " " +
+                                                        job.end_time
+                                                ).format("DD.MM.YYYY HH:mm")}
+                                                <br />
+                                                <Label>Route : </Label>
+                                                {job.from} - {job.to}
+                                                <br />
+                                                <Label>Loknummer : </Label>
+                                                {job.locomotive_nummer}
+                                                <br />
+                                                <Label>Tour : </Label>
+                                                {job.tour_name}
+                                                <br />
+                                                <Label>Zugnummer : </Label>
+                                                {job.zug_nummer}
+                                                <br />
+                                                <Label>Kommentar : </Label>
+                                                {job.description}
+                                            </AccordionContent>
+                                        </AccordionPanel>
+                                    </Accordion>
+                                </div>
+                            ))}
                     </div>
                 </div>
             </div>
