fca14ce3a1ea05576dabb9fc4c1ddc5d3e75c93b
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-07-05 17:15:07 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        dd(config('app.url'));
         \Inertia\Inertia::share('app_url', config('app.url'));
     }
 }
