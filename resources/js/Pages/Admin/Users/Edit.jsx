2ed9d73ad2786362c7b64d8e2f4bdbbff5a3699a
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-08-14 17:39:21 +0200

    test

--- resources/js/Pages/Admin/Users/Edit.jsx
+++ resources/js/Pages/Admin/Users/Edit.jsx
@@ -830,6 +830,26 @@ export default function EditUser({ auth, user_id }) {
                                                     </p>
                                                 )}
                                         </div>
+                                        <div className="mb-4 mx-4">
+                                            <label
+                                                htmlFor="tax_class"
+                                                className="block text-sm font-medium text-gray-700"
+                                            >
+                                                Vergi ID
+                                            </label>
+                                            <Field
+                                                type="number"
+                                                name="tax_id"
+                                                id="tax_id"
+                                                className="placeholder:italic placeholder:text-slate-4000 block bg-white w-full border border-slate-500 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                                            />
+                                            {errors.tax_id &&
+                                                touched.tax_id && (
+                                                    <p className="text-red-500">
+                                                        *{errors.tax_id}
+                                                    </p>
+                                                )}
+                                        </div>
                                     </Tabs.Item>
                                     <Tabs.Item
                                         title="Bonuslar"
