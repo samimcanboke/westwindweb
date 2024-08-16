a5d1473f93e303272e7ae691375189e16cd23f20
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-08-16 15:43:21 +0200

    test

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,12 +2,25 @@ import './bootstrap';
 import '../css/app.css';
 import moment from 'moment';
 import 'moment/dist/locale/de';
+import * as Sentry from "@sentry/react";
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+Sentry.init({
+    dsn: "https://5cf4d82e9e80f727733ebd491c610e81@o4507787257970688.ingest.de.sentry.io/4507787259215952",
+    integrations: [
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+
 
 moment.locale('de');
 
