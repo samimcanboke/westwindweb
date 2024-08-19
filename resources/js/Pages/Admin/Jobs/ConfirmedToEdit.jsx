6cec99a1a242750a4f2aa2dc547b3c253a96ec6a
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-08-19 16:22:52 +0200

    test

--- resources/js/Pages/Admin/Jobs/ConfirmedToEdit.jsx
+++ resources/js/Pages/Admin/Jobs/ConfirmedToEdit.jsx
@@ -208,7 +208,7 @@ export default function ConfirmedToEdit({ auth }) {
                                     type="text"
                                     id="filter"
                                     name="filter"
-                                    placeholder="Filtreleme kriteri girin..."
+                                    placeholder="Lokführer suchen..."
                                     className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
                                     onChange={(e) => {
                                         setFilter({
