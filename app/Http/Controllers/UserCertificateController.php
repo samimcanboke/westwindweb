d46184a6bb43b17645a39d874cb1249b24621cc3
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-08-22 12:39:17 +0200

    test

--- app/Http/Controllers/UserCertificateController.php
+++ app/Http/Controllers/UserCertificateController.php
@@ -16,60 +16,6 @@ class UserCertificateController extends Controller
         return response()->json($userCertificates);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-
-
-        $request->validate([
-            'certificate_id' => 'required',
-            'user_id' => 'required',
-            'certificate_date' => 'required',
-            'validity_date' => 'required',
-            'file' => 'required',
-            'reminder_day' => 'required',
-            'creator'=> 'required',
-            'confirmer'=> 'required',
-            'file' => 'required',
-        ]);
-
-        $userCertificate = UserCertificate::create($request->all());
-        return response()->json(["success" => true, "message" => "Sertifika başarıyla eklendi."], 201);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(UserCertificate $userCertificate)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(UserCertificate $userCertificate)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, UserCertificate $userCertificate)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
