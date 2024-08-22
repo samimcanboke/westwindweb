4106d26965d39c8bd437b86165936148561aed6a
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-08-22 13:06:58 +0200

    tst

--- app/Http/Controllers/UserCertificateController.php
+++ app/Http/Controllers/UserCertificateController.php
@@ -16,6 +16,30 @@ class UserCertificateController extends Controller
         return response()->json($userCertificates);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+
+        $request->validate([
+            'certificate_id' => 'required',
+            'user_id' => 'required',
+            'file' => 'required',
+        ]);
+
+        $userCertificate = UserCertificate::create($request->all());
+        return response()->json(["success" => true, "message" => "Sertifika başarıyla eklendi."], 201);
+    }
 
     /**
      * Remove the specified resource from storage.
