d49ab9a87c95e87104009c7f35ad80928a680399
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-09-11 18:08:14 +0200

    test

--- resources/js/Pages/Admin/Jobs/Planner.jsx
+++ resources/js/Pages/Admin/Jobs/Planner.jsx
@@ -171,6 +171,8 @@ export default function Planner({ auth }) {
             setSelectedUsers(users);
         } else {
             getUsers();
+            setSelectedUsersForTime([]);
+            setSelectedUsers(users);
         }
     }, [selectedClients]);
 
