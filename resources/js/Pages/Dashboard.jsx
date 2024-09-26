5159761239f1cd315573dcdd4c0feb4f3bdfc437
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-09-26 18:57:37 +0200

    test

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,8 +1,48 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import React, { useEffect,useState } from 'react';
+import Chart from "react-apexcharts";
+
 
 export default function Dashboard({ auth }) {
-    console.log(auth);
+
+    const [users, setUsers] = useState([]);
+    const [series, setSeries] = useState({});
+    const getUsers = async () => {
+        const response = await axios.get(route('users.show'));
+        console.log(response.data);
+        setUsers(response.data);
+        setSeries({
+            options: {
+                chart: {
+                    id: "basic-bar"
+                },
+                xaxis: {
+                    categories: response.data.map(user => user.name)
+                }
+            },
+            series: [
+                {
+                      name: "Onaylanmış İşler",
+                      data: Array.from({ length: response.data.length }, () => Math.floor(Math.random() * 100))
+                    },
+                    {
+                      name: "Onay Bekleyen İşler",
+                      data: Array.from({ length: response.data.length }, () => Math.floor(Math.random() * 100))
+                }
+            ]
+        });
+    }
+    
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    useEffect(() => {
+        console.log(series);
+    }, [series]);
+   
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -13,7 +53,15 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        {auth.user.is_admin || auth.user.accountant ? (
+                            series.series && series.series.length > 0 ? (
+                                <Chart options={series.options} series={series.series} type="bar" height={350} />
+                            ) : (
+                                <p>Loading...</p>
+                            )
+                        ) : (
+                            <p>Willkommen! Sie sind nicht berechtigt, diese Seite zu sehen.</p>
+                        )}
                     </div>
                 </div>
             </div>
