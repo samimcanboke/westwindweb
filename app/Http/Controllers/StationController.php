1bc7d12efcb38a5162e97a495866aa4050c22adb
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-09-27 17:43:44 +0200

    test

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -32,6 +32,9 @@ class StationController extends Controller
             $railwayStations = Station::all()->take(10);
             return response()->json($railwayStations);
         } else {
+
+
+            
             $response = Http::get('https://api.openrailwaymap.org/v2/facility?q=' . $request->input('search'));
             $railwayStations = $response->json();    
             if(isset($railwayStations)){
