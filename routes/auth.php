ee4fc63fc52fff452b66181279b53eea08668054
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-11-06 14:30:19 +0100

    test

--- routes/auth.php
+++ routes/auth.php
@@ -63,7 +63,7 @@ Route::middleware('auth')->group(function () {
 
     Route::post('registered', [RegisteredUserController::class, 'store_inside'])->name('registered');
     Route::get('show', [RegisteredUserController::class, 'show'])->name('users.show');
-    Route::get('show_exclude_admins', [RegisteredUserController::class, 'show_exclude_admins'])->name('users.show_exclude_admins');
+    Route::get('show_exclude_admins', [RegisteredUserController::class, 'show_exclude_admins'])->withoutMiddleware([IsAdmin::class])->name('users.show_exclude_admins');
     Route::get('show_all', [RegisteredUserController::class, 'show_all'])->name('users.show_all');
 
 });
