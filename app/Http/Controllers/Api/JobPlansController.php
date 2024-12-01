8ab5b2c1320a06a21674cebe8aff2adffc99f518
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-12-01 18:47:13 +0100

    test

--- app/Http/Controllers/Api/JobPlansController.php
+++ app/Http/Controllers/Api/JobPlansController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\JobPlans;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class JobPlansController extends Controller
 {
     public function index(): JsonResponse
     {
+        dd(Request::all());
         return response()->json(['plans' => JobPlans::all()]);
     }
 
