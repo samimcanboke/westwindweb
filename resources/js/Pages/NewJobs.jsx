e6ee854238df8dacf302c49d2dfa7d3fdf296c8e
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2024-12-04 15:05:47 +0100

    test

--- resources/js/Pages/NewJobs.jsx
+++ resources/js/Pages/NewJobs.jsx
@@ -999,6 +999,120 @@ export default function NewJobs({ auth }) {
                                 </AccordionContent>
                             </AccordionPanel>
                         </Accordion>
+                         <Accordion>
+                            <AccordionPanel />
+                            <AccordionPanel isOpen={false}>
+                                <AccordionTitle className={Object.keys(errors).some(key => ['trainStartPlace', 'trainStartTime', 'trainEndPlace', 'trainEndTime'].includes(key)) ? "text-red-500" : ""}>
+                                    Zug Abfahrt und Ankunft
+                                </AccordionTitle>
+                                <AccordionContent>
+                                    <Label className={errors.trainStartPlace ? "text-red-500" : ""}>Zug Abfahrtsort</Label>
+                                    <LocationField
+                                        id="trainStartPlace"
+                                        name="trainStartPlace"
+                                        type="text"
+                                        placeholder="Zug Abfahrtsort"
+                                        onChange={(e) => {
+                                            setFieldValue(
+                                                "trainStartPlace",
+                                                e.value
+                                            );
+                                        }}
+                                        className={
+                                            errors.trainStartPlace
+
+                                                ? "placeholder:italic placeholder:text-slate-4000 block bg-white w-full border border-red-500 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                                                : "placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                                        }
+                                        selected={values.trainStartPlace}
+                                    />
+                                    {errors.trainStartPlace &&
+                                        (
+                                            <p className="text-red-500">
+                                                *{errors.trainStartPlace}
+                                            </p>
+                                        )}
+                                    <br />
+
+                                    <Label className={errors.trainStartTime ? "text-red-500" : ""}>Zug Abfahrtszeit</Label>
+                                    <div className="flex">
+                                        <input
+                                            type="time"
+                                            id="trainStartTime"
+                                            name="trainStartTime"
+                                            className={
+                                                errors.trainStartTime
+                                                    ? "rounded-none rounded-s-lg bg-gray-50 border text-gray-900 leading-none focus:ring-blue-500 focus:border-blue-500 block flex-1 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                    : "rounded-none rounded-s-lg bg-gray-50 border text-gray-900 leading-none focus:ring-blue-500 focus:border-blue-500 block flex-1 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            }
+                                            value={values.trainStartTime}
+                                            onChange={(e) => {
+                                                setFieldValue(
+                                                    "trainStartTime",
+                                                    e.target.value
+                                                );
+                                            }}
+                                        />
+                                    </div>
+                                    {errors.trainStartTime && (
+                                            <p className="text-red-500">
+                                                *{errors.trainStartTime}
+                                            </p>
+                                        )}
+                                    <Label className={errors.trainEndPlace ? "text-red-500" : ""}>Zug Ankunftsort</Label>
+                                    <LocationField
+                                        id="trainEndPlace"
+                                        name="trainEndPlace"
+                                        type="text"
+                                        placeholder="Zug Ankunftsort"
+                                        onChange={(e) => {
+                                            setFieldValue(
+                                                "trainEndPlace",
+                                                e.value
+                                            );
+                                        }}
+                                        className={
+                                            errors.trainEndPlace
+                                                ? "placeholder:italic placeholder:text-slate-4000 block bg-white w-full border border-red-500 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                                                : "placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                                        }
+                                        selected={values.trainEndPlace}
+                                    />
+                                    {errors.trainEndPlace && (
+                                        <p className="text-red-500">
+                                            *{errors.trainEndPlace}
+                                        </p>
+                                    )}
+                                    <br />
+
+                                    <Label className={errors.trainEndTime ? "text-red-500" : ""}>Zug Ankunftszeit</Label>
+                                    <div className="flex">
+                                        <input
+                                            type="time"
+                                            id="trainEndTime"
+                                            name="trainEndTime"
+                                            className={
+                                                errors.trainEndTime 
+                                                    ? "rounded-none rounded-s-lg bg-gray-50 border text-gray-900 leading-none focus:ring-blue-500 focus:border-blue-500 block flex-1 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                    : "rounded-none rounded-s-lg bg-gray-50 border text-gray-900 leading-none focus:ring-blue-500 focus:border-blue-500 block flex-1 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            }
+                                            value={values.trainEndTime}
+                                            onChange={(e) => {
+                                                setFieldValue(
+                                                    "trainEndTime",
+                                                    e.target.value
+                                                );
+                                            }}
+                                        />
+                                    </div>
+                                    {errors.trainEndTime && (
+                                            <p className="text-red-500">
+                                                *{errors.trainEndTime}
+                                            </p>
+                                        )}
+                                </AccordionContent>
+                            </AccordionPanel>
+                        </Accordion>
                         <Accordion>
                             <AccordionPanel />
                             <AccordionPanel isOpen={false}>
