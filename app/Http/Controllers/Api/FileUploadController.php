22fb6875f12c6d2295dd23db5d3c31720e147dac
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2025-02-20 23:58:26 +0100

    Add debug statement to inspect authenticated user in FileUploadController

--- app/Http/Controllers/Api/FileUploadController.php
+++ app/Http/Controllers/Api/FileUploadController.php
@@ -14,6 +14,7 @@ class FileUploadController extends Controller
     {
         $uploadedFiles = [];
         $user = JWTAuth::user();
+        dd($user);
         if ($request->hasFile('files')) {
             $file = $request->file('files');
             $uniqueId = uniqid();
