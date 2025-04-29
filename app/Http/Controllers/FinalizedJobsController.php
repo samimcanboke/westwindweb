25bb2cbd71f5f79da755359e0e6767feae2ec746
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2025-04-29 20:30:03 +0200

    FinalizedJobsController: Yıllık izin hakları hesaplamasında bir hata düzeltildi; yıllık izin hakları değişkeni doğru şekilde güncellendi.

--- app/Http/Controllers/FinalizedJobsController.php
+++ app/Http/Controllers/FinalizedJobsController.php
@@ -1361,7 +1361,7 @@ class FinalizedJobsController extends Controller
             })
             ->sum() ?? 0;
 
-        $annual_leave_rights = $right_of_annuals - $annual_leave_days;
+        $annual_leave_rights = $right_of_annuals - $annual_leave_rights;
         $data['annual_leave_rights'] = number_format($annual_leave_rights, 2, ',', '');
 
         $annual_leave_days = $user->annualLeaves()
