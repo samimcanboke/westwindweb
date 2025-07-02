f3ab6b415dd7c8f5e54185628cbe61804ebef6ab
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2025-07-02 19:47:17 +0200

    FinalizedJobsController: Kullanıcıların toplam rapor hesaplamasında, yeni kullanıcı ID'leri (33-42) eklenerek raporların kapsamı genişletildi.

--- app/Http/Controllers/FinalizedJobsController.php
+++ app/Http/Controllers/FinalizedJobsController.php
@@ -1313,6 +1313,16 @@ public function get_total_report(Request $request)
             "30" => 0,
             "31" => 0,
             "32" => 0,
+            "33" => 0,
+            "34" => 0,
+            "35" => 0,
+            "36" => 0,
+            "37" => 0,
+            "38" => 0,
+            "39" => 0,
+            "40" => 0,
+            "41" => 0,
+            "42" => 0,
         ];
         $start_of_annual_leave_working_date = null;
         $user_start_working_date = Carbon::parse($user->start_working_date);
