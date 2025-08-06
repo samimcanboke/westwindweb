6933309cb9da21f8ff70ba2a57012ec8356f2037
Samimcan BÖKE <96445882+samimcanboke@users.noreply.github.com>
2025-08-06 18:39:19 +0200

    FinalizedJobsController: Kullanıcıların yıllık izin hakları hesaplamasında hata ayıklama amacıyla dd() fonksiyonu güncellenerek, yıllık izin hakları hesaplaması için daha anlamlı bir çıktı sağlandı; bu sayede hesaplama sürecindeki sorunlar daha kolay tespit edilebilecek.

--- app/Http/Controllers/FinalizedJobsController.php
+++ app/Http/Controllers/FinalizedJobsController.php
@@ -1349,7 +1349,7 @@ public function get_total_report(Request $request)
    
 
         $right_of_annuals = $left_annuals_from_2024[$user->id] + $leave_working_date_left;
-        dd("right_of_annuals",$right_of_annuals);
+      
         
         $annual_leave_rights = $user->annualLeaves()
             ->where('start_date', '>=', Carbon::create($year, 1, 1)->startOfDay()->toDateTimeString())
@@ -1362,6 +1362,7 @@ public function get_total_report(Request $request)
             })
             ->sum() ?? 0;
         $annual_leave_rights = $right_of_annuals - $annual_leave_rights;
+        dd("annual_leave_rights",$annual_leave_rights);
         
         $data['annual_leave_rights'] = number_format($annual_leave_rights, 2, ',', '');
 
